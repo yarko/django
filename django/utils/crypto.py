84acf6e4864ff30337ed36bf552c02a7ffd337ae
Yarko Tymciurak <yarkot@uchicago.edu>
2011-09-10 13:35:31 -0700

    re-structuring crypto.py - not complete, but we need to share what's here now...

--- django/utils/crypto.py
+++ django/utils/crypto.py
@@ -3,7 +3,8 @@ Django's standard crypto functions and utilities.
 """
 
 from django.conf import settings
-from string import digits, letters
+from django.utils.baseconv import BaseConverter
+import string
 import hashlib
 import hmac
 import random
@@ -14,6 +15,23 @@ except NotImplementedError:
 
     
 #ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+# Alphabet naming:
+
+## imported from string:
+# digits
+# lowercase
+# uppercase
+
+ALLCASE_ALPHANUMERIC = string.digits + string.uppercase + string.lowercase
+DIGITS = string.digits
+UPPERCASE = string.uppercase
+LOWERCASE = string.lowercase
+HEX = string.digits + 'abcdef'
+# remove for human consumption - we don't want confusion between letter-O and zero
+# effectively: for i in 'ilIoO01': x.remove(i)
+READABLE_ALPHABET = '23456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijklmnpqrstuvwxyz'
+
 
 def base62_encode(num, alphabet=digits+letters):
     """Encode a number in Base X
@@ -33,31 +51,46 @@ def base62_encode(num, alphabet=digits+letters):
     return ''.join(arr)
 
 
-class Token():
+class Base_Token():
     def __init__(self, value=None, random=False):
         if random:
             self._hash = hashlib.md5(random.getrandbits(256))
         else:
             self._hash = hashlib.md5(value)
 
-    def base16_digest(self, length=None):
+    def base16(self, length=None):
         """
         Outputs our hash to a base 16 string.
         """
         return self._hash.hexdigest()[:length]
 
-    def base62_digest(self, length=None):
+    def base62(self, length=None):
         """
         Outputs our hash to a base 62 string.
         """
         base16 = self._hash.hexdigest()
         base10 = int(base16, 16)
+        
 
-        return base62_encode(base10)[:length]
+        # return base62.encode...
+        # return base62_encode(base10)[:length]
+        
 
     def update(self, value):
         self._hash = self._hash.update(value)
 
+
+class HashToken(BaseToken):
+    """
+    Use for reproducible hash patterns
+    """
+
+class RandomToken(BaseToken):
+    """
+    Use for receiving random hash tokens
+    """
+    
+    
 
 def salted_hmac(key_salt, value, secret=None):
     """
