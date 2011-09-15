7059eace926e0eb9a7f48884fa50fa0b71f37b10
Yarko Tymciurak <yarkot@uchicago.edu>
2011-09-14 19:26:10 -0500

    now using memcached

--- tests/test_sqlite.py
+++ tests/test_sqlite.py
@@ -23,7 +23,11 @@ DATABASES = {
 
 CACHES = {
     'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211',
+    },
+    'other': {
         'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
-        'LOCATION': 'unique-snowflake'
+        'LOCATION': 'unique-snowflake',
     }
 }
